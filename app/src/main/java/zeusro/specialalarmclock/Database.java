118cdde951185107b49ab24a1f71d79d75bf8ff4
867380699 <867380699@qq.com>
2017-08-22 11:08:55 +0800

    update database, add new column repeat_type

--- app/src/main/java/zeusro/specialalarmclock/Database.java
+++ app/src/main/java/zeusro/specialalarmclock/Database.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -18,7 +19,9 @@ import java.util.List;
 import zeusro.specialalarmclock.bean.Alarm;
 
 /**
+ * 数据库
  *
+ * version2时添加了 repeat_type
  * Created by Z on 2015/11/16.
  */
 public class Database extends SQLiteOpenHelper {
@@ -26,7 +29,7 @@ public class Database extends SQLiteOpenHelper {
     private static SQLiteDatabase database = null;
 
     private static final String DATABASE_NAME = "DB";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public static final String ALARM_TABLE = "alarm";
     public static final String COLUMN_ALARM_ID = "_id";
@@ -36,6 +39,32 @@ public class Database extends SQLiteOpenHelper {
     public static final String COLUMN_ALARM_TONE = "alarm_tone";
     public static final String COLUMN_ALARM_VIBRATE = "alarm_vibrate";
     public static final String COLUMN_ALARM_NAME = "alarm_name";
+    public static final String COLUMN_ALARM_REPEAT_TYPE = "alarm_repeat_type";
+
+    private Database(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + ALARM_TABLE + " ( "
+                + COLUMN_ALARM_ID + " INTEGER primary key autoincrement, "
+                + COLUMN_ALARM_ACTIVE + " INTEGER NOT NULL, "
+                + COLUMN_ALARM_TIME + " TEXT NOT NULL, "
+                + COLUMN_ALARM_DAYS + " BLOB NOT NULL, "
+                + COLUMN_ALARM_TONE + " TEXT NOT NULL, "
+                + COLUMN_ALARM_VIBRATE + " INTEGER NOT NULL, "
+                + COLUMN_ALARM_NAME + " TEXT NOT NULL, "
+                + COLUMN_ALARM_REPEAT_TYPE + " INTEGER DEFAULT 0)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if(newVersion>1){
+            String sql = String.format("ALTER TABLE %s ADD COLUMN %s INTEGER DEFAULT 0",ALARM_TABLE,COLUMN_ALARM_REPEAT_TYPE);
+            db.execSQL(sql);
+        }
+    }
 
     public static void init(Context context) {
         if (null == instance) {
@@ -58,26 +87,28 @@ public class Database extends SQLiteOpenHelper {
         instance = null;
     }
 
+    /**
+     * 插入一条数据
+     * @param alarm alarm
+     * @return 新插入数据的pk
+     */
     public static long create(Alarm alarm) {
         ContentValues cv = new ContentValues();
         cv.put(COLUMN_ALARM_ACTIVE, alarm.isActive());
         cv.put(COLUMN_ALARM_TIME, alarm.getAlarmTimeString());
-
         try {
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            ObjectOutputStream oos = null;
-            oos = new ObjectOutputStream(bos);
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
             oos.writeObject(alarm.getDays());
             byte[] buff = bos.toByteArray();
-
             cv.put(COLUMN_ALARM_DAYS, buff);
-
         } catch (Exception e) {
+            e.printStackTrace();
         }
         cv.put(COLUMN_ALARM_TONE, alarm.getAlarmTonePath());
         cv.put(COLUMN_ALARM_VIBRATE, alarm.IsVibrate());
         cv.put(COLUMN_ALARM_NAME, alarm.getAlarmName());
-
+        cv.put(COLUMN_ALARM_REPEAT_TYPE, alarm.getRepeatType());
         return getDatabase().insert(ALARM_TABLE, null, cv);
     }
 
@@ -85,22 +116,20 @@ public class Database extends SQLiteOpenHelper {
         ContentValues cv = new ContentValues();
         cv.put(COLUMN_ALARM_ACTIVE, alarm.isActive());
         cv.put(COLUMN_ALARM_TIME, alarm.getAlarmTimeString());
-
         try {
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             ObjectOutputStream oos = null;
             oos = new ObjectOutputStream(bos);
             oos.writeObject(alarm.getDays());
             byte[] buff = bos.toByteArray();
-
             cv.put(COLUMN_ALARM_DAYS, buff);
-
         } catch (Exception e) {
+            e.printStackTrace();
         }
         cv.put(COLUMN_ALARM_TONE, alarm.getAlarmTonePath());
         cv.put(COLUMN_ALARM_VIBRATE, alarm.IsVibrate());
         cv.put(COLUMN_ALARM_NAME, alarm.getAlarmName());
-
+        cv.put(COLUMN_ALARM_REPEAT_TYPE, alarm.getRepeatType());
         return getDatabase().update(ALARM_TABLE, cv, "_id=" + alarm.getId(), null);
     }
 
@@ -124,42 +153,49 @@ public class Database extends SQLiteOpenHelper {
                 COLUMN_ALARM_DAYS,
                 COLUMN_ALARM_TONE,
                 COLUMN_ALARM_VIBRATE,
-                COLUMN_ALARM_NAME
+                COLUMN_ALARM_NAME,
+                COLUMN_ALARM_REPEAT_TYPE
         };
         Cursor c = getDatabase().query(ALARM_TABLE, columns, COLUMN_ALARM_ID + "=" + id, null, null, null,
                 null);
         Alarm alarm = null;
 
         if (c.moveToFirst()) {
-
-            alarm = new Alarm();
-            alarm.setId(c.getInt(0));
-            alarm.setActive(c.getInt(1) == 1);
-            alarm.setAlarmTime(c.getString(2));
-            byte[] repeatDaysBytes = c.getBlob(3);
-
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(repeatDaysBytes);
-            try {
-                ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-                int[] repeatDays;
-                Object object = objectInputStream.readObject();
-                if (object instanceof int[]) {
-                    repeatDays = (int[]) object;
-                    alarm.setDays(repeatDays);
-                }
-            } catch (StreamCorruptedException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-
-            alarm.setAlarmTonePath(c.getString(4));
-            alarm.setVibrate(c.getInt(5) == 1);
-            alarm.setAlarmName(c.getString(6));
+            alarm = readOneRow(c);
         }
         c.close();
+        return alarm;
+    }
+
+    @NonNull
+    private static Alarm readOneRow(Cursor c) {
+        Alarm alarm = new Alarm();
+        alarm.setId(c.getInt(c.getColumnIndexOrThrow(COLUMN_ALARM_ID)));
+        alarm.setActive(c.getInt(c.getColumnIndexOrThrow(COLUMN_ALARM_ACTIVE)) == 1);
+        alarm.setAlarmTime(c.getString(c.getColumnIndexOrThrow(COLUMN_ALARM_TIME)));
+        byte[] repeatDaysBytes = c.getBlob(c.getColumnIndexOrThrow(COLUMN_ALARM_DAYS));
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(repeatDaysBytes);
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            int[] repeatDays;
+            Object object = objectInputStream.readObject();
+            if (object instanceof int[]) {
+                repeatDays = (int[]) object;
+                alarm.setDays(repeatDays);
+            }
+        } catch (StreamCorruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        alarm.setAlarmTonePath(c.getString(c.getColumnIndexOrThrow(COLUMN_ALARM_TONE)));
+        alarm.setVibrate(c.getInt(c.getColumnIndexOrThrow(COLUMN_ALARM_VIBRATE)) == 1);
+        alarm.setAlarmName(c.getString(c.getColumnIndexOrThrow(COLUMN_ALARM_NAME)));
+        alarm.setRepeatType(c.getInt(c.getColumnIndexOrThrow(COLUMN_ALARM_REPEAT_TYPE)));
         return alarm;
     }
 
@@ -171,70 +207,20 @@ public class Database extends SQLiteOpenHelper {
                 COLUMN_ALARM_DAYS,
                 COLUMN_ALARM_TONE,
                 COLUMN_ALARM_VIBRATE,
-                COLUMN_ALARM_NAME
+                COLUMN_ALARM_NAME,
+                COLUMN_ALARM_REPEAT_TYPE
         };
         return getDatabase().query(ALARM_TABLE, columns, null, null, null, null,
                 null);
-    }
-
-    private Database(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + ALARM_TABLE + " ( "
-                + COLUMN_ALARM_ID + " INTEGER primary key autoincrement, "
-                + COLUMN_ALARM_ACTIVE + " INTEGER NOT NULL, "
-                + COLUMN_ALARM_TIME + " TEXT NOT NULL, "
-                + COLUMN_ALARM_DAYS + " BLOB NOT NULL, "
-                + COLUMN_ALARM_TONE + " TEXT NOT NULL, "
-                + COLUMN_ALARM_VIBRATE + " INTEGER NOT NULL, "
-                + COLUMN_ALARM_NAME + " TEXT NOT NULL)");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + ALARM_TABLE);
-        onCreate(db);
     }
 
     public static List<Alarm> getAll() {
         List<Alarm> alarms = new ArrayList<Alarm>();
         Cursor cursor = Database.getCursor();
         if (cursor.moveToFirst()) {
-
             do {
-
-                Alarm alarm = new Alarm();
-                alarm.setId(cursor.getInt(0));
-                alarm.setActive(cursor.getInt(1) == 1);
-                alarm.setAlarmTime(cursor.getString(2));
-                byte[] repeatDaysBytes = cursor.getBlob(3);
-
-                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(repeatDaysBytes);
-                try {
-                    ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-                    int[] repeatDays;
-                    Object object = objectInputStream.readObject();
-                    if (object instanceof int[]) {
-                        repeatDays = (int[]) object;
-                        alarm.setDays(repeatDays);
-                    }
-                } catch (StreamCorruptedException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                alarm.setAlarmTonePath(cursor.getString(4));
-                alarm.setVibrate(cursor.getInt(5) == 1);
-                alarm.setAlarmName(cursor.getString(6));
-
+                Alarm alarm = readOneRow(cursor);
                 alarms.add(alarm);
-
             } while (cursor.moveToNext());
         }
         cursor.close();
