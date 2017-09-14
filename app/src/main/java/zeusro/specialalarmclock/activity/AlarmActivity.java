92a3fdbf7143c04809b05a391b346aa5955ddb66
moofie <406110076@qq.com>
2017-09-14 17:53:29 +0800

    cancel exit when press back btn

--- app/src/main/java/zeusro/specialalarmclock/activity/AlarmActivity.java
+++ app/src/main/java/zeusro/specialalarmclock/activity/AlarmActivity.java
@@ -150,19 +150,7 @@ public class AlarmActivity extends BaseActivity{
 
     @Override
     public void onBackPressed() {
-        if (!isExit) {
-            isExit = true; // 准备退出
-            Toast.makeText(this, "再按一次退出", Toast.LENGTH_SHORT).show();
-            Timer tExit = new Timer();
-            tExit.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    isExit = false; // 取消退出
-                }
-            }, 2000); // 如果2秒钟内没有按下返回键，则启动定时器取消掉刚才执行的任务
-        } else {
-            finish();
-        }
+        moveTaskToBack(false);
     }
 
     private void createNotification(Alarm alarm) {
